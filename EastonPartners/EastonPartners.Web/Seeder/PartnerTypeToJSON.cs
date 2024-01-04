0057684807c62d81974153504db0b691a5c57f09
G0lden <samirissaayman@gmail.com>
2024-01-04 18:48:39 -0500

    Finished synchronizing the paartnertypes datatable

---
+++ EastonPartners/EastonPartners.Web/Seeder/PartnerTypeToJSON.cs
@@ -0,0 +1,34 @@
+﻿using EastonPartners.Infrastructure.Persistence.DbContexts;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace EastonPartners.Web.Seeder
+{
+    public class PartnerTypeToJSON
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PartnerTypeToJSON(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public void copyToJSON()
+        {
+            //Retrieve data from the database
+            var data = _context.PartnerType.ToList();
+
+            //Directory to save the file
+            var filePath = ".\\Seeder\\partnertypes.json";
+
+            //Serialize data to JSON
+            var jsonData = JsonConvert.SerializeObject(data, Formatting.Indented);
+
+            //Save JSON data to a file
+            File.WriteAllText(filePath, jsonData);
+        }
+    }
+}
