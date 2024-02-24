fdf1327fc8d052b22e9f6e673c6318d916075bd9
G0lden <samirissaayman@gmail.com>
2024-02-23 19:55:35 -0500

    Placed the code that creates a paragraph in its own function

--- EastonPartners/EastonPartners.Web/wwwroot/vendor/chat-bot/bot.js
+++ EastonPartners/EastonPartners.Web/wwwroot/vendor/chat-bot/bot.js
@@ -16,9 +16,7 @@ function go() {
 
     //Answers how to log in
     if (message.toLowerCase().includes("login") || message.toLowerCase().includes("log in") || (message.toLowerCase().includes("access") && message.toLowerCase().includes("account"))) {
-        const assisText = document.createElement("p");
-        assisText.innerText = "Assistant: ";
-        assisText.style = "color: red;";
+        const assisText = response(isFound);
 
         assisText.innerText += "To login to your RoverPartners account, you can: \n 1. Go to the Homepage \n 2. Click on Login \n 3. Enter your email and password then hit enter. \n";
         isFound = true;
@@ -27,10 +25,7 @@ function go() {
 
     //Tells user that they can't create an account for themselves
     if (((message.toLowerCase().includes("create") || message.toLowerCase().includes("make")) && message.toLowerCase().includes("account")) || message.toLowerCase().includes("register")) {
-        const assisText = document.createElement("p");
-        if (isFound) assisText.innerText = "";
-        else assisText.innerText = "Assistant: ";
-        assisText.style = "color: red;";
+        const assisText = response(isFound);
 
         assisText.innerText += "You can't create an account or register on this app. You must have an account created by the school district in order to use RoverPartners. If you already have an account, login with your email and password. \n";
         isFound = true;
@@ -39,10 +34,7 @@ function go() {
 
     //Answers how to view the partner information
     if ((message.toLowerCase().includes("view") || message.toLowerCase().includes("manage") || message.toLowerCase().includes("edit")) && message.toLowerCase().includes("partner")) {
-        const assisText = document.createElement("p");
-        if (isFound) assisText.innerText = "";
-        else assisText.innerText = "Assistant: ";
-        assisText.style = "color: red;";
+        const assisText = response(isFound);
 
         assisText.innerText += "If you're logged in, go to the homepage and click on the button that says \"Dashboard.\" In your dashboard you should see an option that says \"Partners,\" and if you click on it you should be able to view and manage the information about all the partners. \nNOTE: If you're not an admin you can only view the information but not modify it. \n";
         isFound = true;
@@ -51,10 +43,7 @@ function go() {
 
     //Answers how to manage users and roles
     if ((message.toLowerCase().includes("manage") || message.toLowerCase().includes("edit")) && (message.toLowerCase().includes("user") || message.toLowerCase().includes("role"))) {
-        const assisText = document.createElement("p");
-        if (isFound) assisText.innerText = "";
-        else assisText.innerText = "Assistant: ";
-        assisText.style = "color: red;";
+        const assisText = response(isFound);
 
         assisText.innerText += "If you're an admin, and you'relogged in, go to the homepage and click on the button that says \"Dashboard.\" In your dashboard you should see two options, \"Users,\" and \"Roles.\" Click on one of them and you should be able to view, create, edit, and delete users or roles from the app. \nNOTE: If you're not an admin you don't have access to manage users or roles. \n";
         isFound = true;
@@ -63,9 +52,17 @@ function go() {
 
     //If no keywords are found in questions, ask user to rephrase question
     if (!isFound) {
-        const assisText = document.createElement("p");
-        assisText.innerText = "Assistant: Sorry I didn't understand. Can you rephrase your question? \n";
-        assisText.style = "color: red;";
+        const assisText = response(isFound);
+        assisText.innerText += "Sorry I didn't understand. Can you rephrase your question? \n";
         chat.appendChild(assisText);
     }
+}
+
+//Creates and returns a paragraph HTML element that represents the assistant's response
+function response(isFound) {
+    const assisText = document.createElement("p");
+    if (isFound) assisText.innerText = "";
+    else assisText.innerText = "Assistant: ";
+    assisText.style = "color: red;";
+    return assisText;
 }
