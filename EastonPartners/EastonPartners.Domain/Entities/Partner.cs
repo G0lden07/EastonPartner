71539dcad08d3523b6f1b665393380090475e4f2
G0lden <samirissaayman@gmail.com>
2024-03-09 16:10:03 -0500

    Added input validation for email and phone number fields
    Added input validation for email and phone number fields in the partners model

--- EastonPartners/EastonPartners.Domain/Entities/Partner.cs
+++ EastonPartners/EastonPartners.Domain/Entities/Partner.cs
@@ -28,11 +28,15 @@ namespace EastonPartners.Domain.Entities
         public string Description { get; set; }
 
 		[Display(Name = "Phone Number")]
-		public string PhoneNumber { get; set; }
+        [RegularExpression(@"^\+?(\d[\d-. ]+)?(\([\d-. ]+\))?[\d-. ]+\d$", ErrorMessage = "Invalid Phone Number")]
+        public string PhoneNumber { get; set; }
+
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
         public string Email { get; set; }
         public string Address { get; set; }
         public string City { get; set; }
         public string State { get; set; }
+
 		[Display(Name = "Postal Code")]
 		public string PostalCode { get; set; }
         public string Website { get; set; }
